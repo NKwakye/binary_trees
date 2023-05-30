17e49c1e09f457d4ec079a1af8240e84987189d8
Nana Kwakye Owusu <dkwakye94@gmail.com>
2023-05-30 03:52:35 -0700

    ADVANCED TASK

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: pointer to the node to check
@@ -11,6 +12,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (1);
 	return (0);
 }
+
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height of
@@ -20,6 +22,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
+
 	if (tree == NULL || binary_tree_is_leaf(tree))
 		return (0);
 	left = binary_tree_height(tree->left);
